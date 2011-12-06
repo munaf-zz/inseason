96464a91bf870c1ec20206d2e0b0a7d0a7e7177d
Munaf Assaf <munaf.assaf@gmail.com>
2011-12-05 23:57:46 -0500

    Boilerplate for FatSecret OAuth

--- js/fatsecret.js
+++ js/fatsecret.js
@@ -1,8 +1,23 @@
 // FatSecret API
 // http://platform.fatsecret.com/api/Default.aspx?screen=rapih
+// Uses jsOAuth: http://bytespider.github.com/jsOAuth/
+
+var FS_API = "http://platform.fatsecret.com/rest/server.api";
+
+var FS_CONFIG = {
+  consumerKey:    "4d8469cc5a794a91abab7e12b6f625b7",
+  consumerSecret: "6216b13c7b464447b8fe9149ff5e5394",
+};
+
+var FS = new OAuth(FS_CONFIG);
 
 // Returns food ID based on search term
-function fs_food_search(term) {
+function fs_food_search(str) {
+  var url = FS_API + "?format=json&method=foods.search&search_expression="+str;
+  var id = null;
+  FS.get(url, function(data) {
+    // Process JSON response
+  });
 }
 
 // Returns nutrition info using food ID 
@@ -11,4 +26,9 @@ function fs_nutrition(food_id) {
 
 // Returnns array of nutrition info based on array of food IDs
 function fs_all_nutritions(foodarray) {
+}
+
+function fs_failure(data) {
+  console.error(data)
+  alert("Can't connect to FatSecret API (see console)");
 }

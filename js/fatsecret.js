e0e5daa781d563db290d7bf79b44fa5ecf2b093e
Naim Falandino <lnfalandino@gmail.com>
2011-12-13 16:01:53 -0500

    Additional comments on why I think the FS API is broken.

--- js/fatsecret.js
+++ js/fatsecret.js
@@ -6,6 +6,7 @@
 
 var FS_API = "http://platform.fatsecret.com/rest/server.api";
 
+// Replaced Munaf's (seemingly broken) API keys with my own.
 var FS_CONFIG = {
   consumerKey:    "af74e371df6944b1872993af862cc2d2",
   consumerSecret: "b37852881e45426f870067ef7c313d50",
@@ -20,12 +21,20 @@ function fs_food_search(str) {
   var id = null;
   // Part of the problem with FatSecret + OAuth has to do with CORS. I think the
   // browser is not accepting the response (or not allowing it in the first place).
-  // You can tell because if you look in the Network tab you'll see the request
-  // status marked as cancelled. You can start Chrome in a no-security sandbox mode
-  // that is useful for dev using:
+  // You can tell because if you look in the dev console network tab you'll see the
+  // request status marked as cancelled. You can start Chrome in a no-security
+  // sandbox mode that is useful for dev using:
+  //
   // /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-web-security --allow-file-access-from-files --allow-file-access --user-data-dir=chrome-test
+  //
+  // NOTE: This will create a user profile directory for Chrome called chrome-test
+  // in the pwd.
+  //
   // When you make the request under that instance instead of a 403 you will get a
-  // response with an error saying that oauth_signature_method is missing.
+  // response with an error saying that oauth_signature_method is missing. That's
+  // BS though because if you check the headers it is most definitely there and it
+  // is set correctly, so I'm not sure what the deal is. All I know is I've spent
+  // way too much time trying to get this thing to work. Not sure what to try next.
   var ops = {
   	method: 'GET',
   	url: url,

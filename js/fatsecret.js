53de00e3cc6eb5460d728ec763c7e2c3ccaf3256
Naim Falandino <lnfalandino@gmail.com>
2011-12-15 11:51:53 -0500

    Fetching and caching food objects
    Call fs_getFoods with your list of foods and callback.

--- js/fatsecret.js
+++ js/fatsecret.js
@@ -12,9 +12,78 @@ var FS_CONFIG = {
 
 var FS = new OAuth(FS_CONFIG);
 
+// "Cache" object for foods
+var FOOD_CACHE = new Array();
+
+function fs_getFoods(foods, clientCallback) {
+  
+  for (i in foods) {
+    
+    if (FOOD_CACHE[foods[i]]) {
+      console.log("Cache hit on " + foods[i]);
+      clientCallback(FOOD_CACHE[foods[i]]);
+    }
+    else {
+      console.log("Querying FS for " + foods[i]);
+      fs_foodSearch(foods[i], clientCallback);
+    }
+  }
+}
+
 // Returns food ID based on search term
-function fs_food_search(food) {
-  var id = null;
+function fs_foodSearch(clientFood, clientCallback) {
+  
+  fs_call('foods.search', {'search_expression': clientFood}, function(data) {
+    
+	  var result = JSON.parse(data.text);
+	  
+	  if (result.error) {
+	    console.log("Error in fs_foodSearch");
+	    console.log(result.error.message);
+	    alert("There was a problem with the FatSecret API (see console)");
+	    return;
+	  }
+	  
+	  var foodList = result.foods.food;
+	  var foodRE = new RegExp(clientFood, 'i');
+	  
+	  // Find the best match and pass it on to getFood
+	  for (i in foodList) {
+	    
+	    // Get item that matches the food as given and has generic type
+	    if (foodList[i].food_name.match(foodRE) &&
+	        foodList[i].food_type == "Generic") {
+	      console.log("Found match for " + clientFood + " with id=" + foodList[i].food_id);
+	      fs_getFood(foodList[i], clientFood, clientCallback);
+	      break;
+	    }
+	  }
+	});
+}
+
+function fs_getFood(foodObj, clientFood, clientCallback) {
+  
+  fs_call('food.get', {'food_id': foodObj.food_id}, function(data) {
+    
+    var result = JSON.parse(data.text);
+	  
+	  if (result.error) {
+	    console.log("Error in fs_getFood");
+	    console.log(result.error.message);
+	    alert("There was a problem with the FatSecret API (see console)");
+	    return;
+	  }
+	  
+    console.log("Storing " + foodObj.food_name + " (clientName=" + clientFood + ") in cache");
+    FOOD_CACHE[clientFood] = result;
+    
+    clientCallback(result);    
+  });
+}
+
+// Performs a request to the FS API for the given method
+function fs_call(method, methodData, callback) {
+  
   // Part of the problem with FatSecret + OAuth has to do with CORS. The browser is
   // not accepting the response (or not allowing it in the first place).
   // You can tell because if you look in the dev console network tab you'll see the
@@ -25,14 +94,21 @@ function fs_food_search(food) {
   //
   // NOTE: This will create a user profile directory for Chrome called chrome-test
   // in the pwd.
+  
+  var data = {
+    'format': 'json',
+	  'method': method
+  }
+  
+  // Copy in methodData
+  for (key in methodData)
+    data[key] = methodData[key];
+  
   var ops = {
-    // I have modified jsOAuth so that both GET and POST work correctly now.
+    // Modified jsOAuth so GET and POST both work.
   	method: 'POST',
   	url: FS_API,
-  	success: function(data) {
-  	  console.log(JSON.parse(data.text));
-  	  console.log(JSON.parse(data.text).foods.food[0].food_description);
-  	},
+  	success: callback,
   	failure: function(data) {
   	  console.error(JSON.parse(data.text));
       alert("Can't connect to FatSecret API (see console)");
@@ -41,19 +117,8 @@ function fs_food_search(food) {
   	  // CORS is still an issue. Best bet is to run with security disabled for now.
   		'Access-Control-Allow-Origin': '*'
   	},
-  	data: {
-  	  'format': 'json',
-  	  'method': 'foods.search',
-  	  'search_expression': food
-  	}
+  	data: data
   };
+  
   FS.request(ops);
-}
-
-// Returns nutrition info using food ID
-function fs_nutrition(food_id) {
-}
-
-// Returnns array of nutrition info based on array of food IDs
-function fs_all_nutritions(foodarray) {
 }
